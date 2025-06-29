7a3f5c31b2be33a53446cb3443631e731efd97c9
Nikhilesh24-git <nikhileshreddy24@gmail.com>
2025-06-29 16:50:45 +0530

    First1

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import { Container } from "@mui/material";
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <Container className="ft">
+        <p>Copyright© 2025 All Rights Reserved| Archit & Adarsh</p>
+      </Container>
+    </div>
+  );
+};
+
+export default Footer;
